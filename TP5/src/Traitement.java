8aee08e2a5773e8c1e5d395a8b13dcd7b1baa0cc
adrienED <adrien.chabas@gmail.com>
2018-10-17 18:37:58 +0200

    ajout TP5

---
+++ TP5/src/Traitement.java
@@ -0,0 +1,32 @@
+
+public class Traitement {
+
+
+
+		
+		
+	public static String getAdresseIP (String S){
+		int n;
+		String ip="";
+		 n= S.indexOf(" ");
+		 
+		for (int i=0;i<n;i++) {
+			ip+=S.charAt(i);
+		}
+		
+		return ip;
+		
+	}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+
---
+++ TP5/src/principalTraitement.java
@@ -0,0 +1,17 @@
+
+public class principalTraitement {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+	
+	System.out.println(Traitement.getAdresseIP("195.242.73.130  hfjdsk"));
+	
+	
+	}
+	
+	
+	
+
+}
