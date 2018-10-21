fac6d0ab3d9b85dcf257512813aa26cc78fd85a8
adrienED <adrien.chabas@gmail.com>
2018-10-21 18:34:33 +0200

    reecriture TD5

--- TD5/src/TD5.java
+++ TD5/src/TD5.java
@@ -2,9 +2,14 @@
 public class TD5 {
 
 	public static void main(String[] args) {
-		//affiche ("UE info ");
+		//afficheP ("UE info ");
 		//String R = AjouteEtoile("UE info",3);
-		//System.out.println(R);
+		//System.out.println(SupprimeCar("chocolat",'o'));
+		//System.out.println("Duval".substring(1,3));
+		//System.out.println("Marguerite".indexOf("rg"));
+		//System.out.println("multiplication".indexOf("ti",4));
+		//System.out.println("banane".indexOf("an",4));
+	
 	}
 		
 		public static void affiche (String S) {
@@ -15,7 +20,24 @@ public class TD5 {
 					}
 			}
 		}
+		
+		public static void afficheP (String S) {
+			String C="";
+			for (int i=0;i<S.length();i++) {
+				
+					if(i<S.length()-2) {
+						C=C+S.charAt(i)+',';
+						
+					}
+					else
+					C=C+S.charAt(i);
+			}
+			
+			System.out.println(C);
+		}
 
+		
+		
 public static String AjouteEtoile (String S, int n) {
 	String E= "";
 	for (int k = 0;k<n;k++) 
@@ -33,11 +55,12 @@ public static String AjouteEtoile (String S, int n) {
 private static String [] R;
 
 
-/*public static String SupprimeCar (String [] S, char c) {
+public static String SupprimeCar (String S, char c) {
 	
+	String R="";
 
-	for (int k=0;k<S.length;k++) {
-		if (S.chartAt(k)==c) {
+	for (int k=0;k<S.length();k++) {
+		if (S.charAt(k)!=c) {
 		R=R+S.charAt(k);
 		
 	}
@@ -47,8 +70,8 @@ private static String [] R;
 	
 
 	
-//}
-*/
+}
+
 
 }
 
