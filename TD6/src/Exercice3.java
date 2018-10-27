aa6645342141a77f4a43e011d8f42799d8040a2a
adrienED <adrien.chabas@gmail.com>
2018-10-27 16:02:32 +0200

    Ajout Exercice3

---
+++ TD6/src/Exercice3.java
@@ -0,0 +1,18 @@
+
+public class Exercice3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//int s = somme (458);
+		System.out.println(s);
+	}
+	
+	public static int somme (int n) {
+		
+		if (n==0) return n;
+		else
+		return n+somme(n);
+	}
+	
+
+}
