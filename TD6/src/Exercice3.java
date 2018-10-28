1024f55bcb6a2cbda29ddac062bce846d6f2312b
adrienED <adrien.chabas@gmail.com>
2018-10-28 14:07:55 +0100

    finalisation exercice3

--- TD6/src/Exercice3.java
+++ TD6/src/Exercice3.java
@@ -3,16 +3,28 @@ public class Exercice3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//int s = somme (458);
+		int s = somme (458);
 		System.out.println(s);
+		System.out.println(estDiv3(451));
 	}
 	
 	public static int somme (int n) {
 		
-		if (n==0) return n;
+		if (n%10==n) return n;
 		else
-		return n+somme(n);
+			
+		return n%10+somme(n/10);
 	}
 	
-
+	public static boolean estDiv3 (int n) {
+		
+		if (n<10 && (n==3 || n==6 || n==9)) return true;
+		else {if (somme(n)%3==0) { return true;
+		
+		}
+				else {return false;
+		}
+		
+	}
+}
 }
